1f8bfeda472598b918826eba6d4a0675fe7a1204
ledlamp <ledlamp89@gmail.com>
2018-05-20 15:26:38 -0700

    Allow re-bridging (#1)

--- src/mppbridger.js
+++ src/mppbridger.js
@@ -372,12 +372,25 @@ commands.bridge = {
 		var room = msg.txt(1);
 		if (!room) return false;
 		var existingBridge = (await dbClient.query('SELECT * FROM bridges WHERE mpp_room = $1;', [room])).rows[0];
-		if (existingBridge && !existingBridge.disabled) return msg.reply(`${site} room ${room} is already bridged.`);
-		else if ((existingBridge && existingBridge.disabled) || config.disabledRooms.includes(room)) return msg.reply(`You cannot bridge this room.`);
+		if (existingBridge) {
+			if (!existingBridge.disabled) {
+				return msg.reply(`${site} room ${room} is already bridged.`);
+			} else {
+				if (config.disabledRooms.includes(room)) {
+					return msg.reply(`You cannot bridge this room.`);
+				} else /* rebridge */ {
+					await dbClient.query("UPDATED BRIDGES SET disabled = false WHERE mpp_room = $1", [room]);
+					createMPPbridge(room, existingBridge.mpp_room, existingBridge.site, existingBridge.webhook_id, existingBridge.webhook_token);
+					await msg.reply(`${site} room ${room} has been re-bridged.`);
+					return;
+				}
+			}
+		}
+		/* new bridge */
 		var discordChannelName = room.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase();
 		var categoryID = '360557444952227851';
 		var channel = await dClient.guilds.get(config.guildID).channels.create(discordChannelName, {parent: categoryID});
-		channel.setTopic(`Bridged to http://www.multiplayerpiano.com/${encodeURIComponent(room)}`);
+		channel.setTopic(`http://www.multiplayerpiano.com/${encodeURIComponent(room)}`);
 		var webhook = await channel.createWebhook('Webhook');
 		createMPPbridge(room, channel.id, site, webhook.id, webhook.token);
 		dbClient.query('INSERT INTO bridges (site, mpp_room, discord_channel_id, webhook_id, webhook_token, owner_discord_user_id) VALUES ($1, $2, $3, $4, $5, $6)', [
