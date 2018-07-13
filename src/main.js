2fd27fc5f15f18984359f87b03b559bd0e779263
Lamp <ledlamp89@gmail.com>
2018-07-13 16:37:15 -0700

    rm dat discord console sheit

--- src/main.js
+++ src/main.js
@@ -5,26 +5,6 @@ global.fs = require('fs');
 global.config = require('./config');
 global.dClient = new Discord.Client({ disableEveryone: true });
 
-console._log = console.log;
-console.log = function(){
-	console._log.apply(console, arguments);
-	log2discord(arguments);
-}
-console._error = console.error;
-console.error = function(){
-	console._error.apply(console, arguments);
-	log2discord(arguments);
-}
-console.warn = console.error;
-console.info = console.log;
-
-var webhook = new Discord.WebhookClient(config.webhooks.console[0], config.webhooks.console[1]);
-function log2discord(str){
-	str = Array.from(str);
-	str = str.map(require('util').inspect);
-	str = str.join(' ');
-	webhook.send(str, {split:{char:''}});
-}
 
 process.on('unhandledRejection', (reason, promise) => {
 	console.error(promise);
